c9e376c10eeedf7ec610a20b172a03852d558bfd
frantisek-boura <widowka01@gmail.com>
2024-07-04 00:02:43 +0200

    completely done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,21 @@ const App = () => {
     const [uploading, setUploading] = useState(false)
     const [uploadMessage, setUploadMessage] = useState("")
     const [loading, setLoading] = useState(true)
-    const [connectionError, setConnectionError] = useState(true)
+    const [connectionError, setConnectionError] = useState(false)
+
+    const loadData = () => {
+        setLoading(true)
+
+        axios.get("http://localhost:8000/api/items")
+        .then((response) => {
+            setData(response.data)
+            setConnectionError(false)
+        }).catch(() => {
+            setConnectionError(true)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }
 
     const submitFile = (e) => {
         e.preventDefault()
@@ -15,44 +29,41 @@ const App = () => {
         let formData = new FormData(e.target)
         let file = formData.get('file')
 
-        if (!file) console.error('Incorrect file')
+        if (!file) {
+            console.error('Incorrect file')
+            return
+        } 
 
         setUploading(true)
-        axios.post("localhost:8000/api/items", formData, { 
+        axios.post("http://localhost:8000/api/items", formData, { 
             headers: {
                 'Content-Type': 'multipart/form-data'
             } 
         }).then(() => {
             setUploadMessage("Data uploaded successfully");
+            setConnectionError(false)
         }).catch(() => {
             setUploadMessage("There was an error while uploading the data");
             setConnectionError(true)
+        }).finally(() => {
+            setUploading(false)
+            loadData()
         })
 
-        setUploading(false)
     }
 
     useEffect(() => {
-        setTimeout(() => {
-            if (uploadMessage.length > 0) setUploadMessage("")
-        }, 5000);
+        if (uploadMessage.length > 0) {
+            const timeout = setTimeout(() => {
+                setUploadMessage("")
+            }, 5000);
+
+            return () => clearTimeout(timeout);
+        }
     }, [uploadMessage])
 
-    useEffect(() => {
-    
-        setLoading(true)
-        axios.get("http://localhost:8000/api/items")
-        .then((response) => {
-            setData(response.data)
-            console.log(response.data)
-            setConnectionError(false)
-            setLoading(false)
-        }).catch(() => {
-            setData([])
-            setConnectionError(true)
-            setLoading(false)
-        });
-
+    useEffect(() => { 
+        loadData()
     }, [])
 
     return (
@@ -65,38 +76,42 @@ const App = () => {
             {connectionError && <h3>Nelze navázat spojení se serverem</h3>}
             {(!connectionError && uploading) && <h3>Nahrávání...</h3>}
             {(!connectionError && uploadMessage.length > 0) && <h3>{uploadMessage}</h3>}
-            {(!connectionError && loading) && <h3>Načítání...</h3>}
-            {(!connectionError && data.length == 0) && <h3>Nejsou nahraná žádná data</h3>}
-            {(!connectionError && data.length > 0) && 
+            {(!connectionError && loading && !uploading) && <h3>Načítání...</h3>}
+            {(!connectionError && data.length == 0 && !uploading && !loading) && <h3>Nejsou nahraná žádná data</h3>}
+            {(!connectionError && data.length > 0 && !uploading && !loading) && 
                 <table>
-                    <th>
-                        <td>Code</td>
-                        <td>EAN13</td>
-                        <td>DUN14</td>
-                        <td>Carton_Qty</td>
-                        <td>Price</td>
-                        <td>Weight(kg)</td>
-                        <td>Size</td>
-                        <td>Colour Name</td>
-                        <td>Image</td>
-                        <td>Description</td>
-                    </th>
-                    {data.map((item, index) => {
-                        return (
-                            <tr key={index}>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                        )
-                    })}
+                    <thead>
+                        <tr>
+                            <th>Code</th>
+                            <th>EAN13</th>
+                            <th>DUN14</th>
+                            <th>Carton_Qty</th>
+                            <th>Price</th>
+                            <th>Weight(kg)</th>
+                            <th>Size</th>
+                            <th>Colour Name</th>
+                            <th>Image</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.code}</td>
+                                    <td>{item.ean13}</td>
+                                    <td>{item.dun14}</td>
+                                    <td>{item.cartonQty}</td>
+                                    <td>{item.price}</td>
+                                    <td>{item.weight}</td>
+                                    <td>{item.size}</td>
+                                    <td>{item.colour}</td>
+                                    <td><img src={item.imagePath} width={100} height={100} alt={item.imagePath}/></td>
+                                    <td>{item.description}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
                 </table>
             }
         </>
